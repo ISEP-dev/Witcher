0a7fd3e03ebf3afcaf84ae915035a8f29f406aa6
Perrault <lucas.perrault@hotmail.com>
2020-10-20 14:46:10 +0200

    Initial commit with teacher files

---
+++ src/witcher.js
@@ -0,0 +1,12 @@
+import { resistances } from './resistances'
+import LivingThing from './livingThing'
+
+class Witcher extends LivingThing {
+  constructor(disciple) {
+    super(disciple.name, resistances.SILVER, 10)
+
+    this.efficiency = disciple.mushroomsAte * disciple.trainingSessions
+  }
+}
+
+export default Witcher
